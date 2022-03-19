aaa4637ffaeff1952200914b77a99cba5caf83c3
Uladzislau Khamkou <kham@tuta.io>
2022-03-19 22:45:28 +0300

    implement graph inversion

--- graphs.py
+++ graphs.py
@@ -1,2 +1,10 @@
+def graph_inverse(graph):
+    G_inv = {}
+    for u, vs in graph.items():
+        for v in vs:
+            G_inv.setdefault(v, set()).add(u)
+    return G_inv
+
+
 if __name__ == "__main__":
     print("Non! Vous n'utilisez pas le module graphique correctement.")
