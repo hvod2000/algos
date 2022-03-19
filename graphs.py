7836069d53a93e1c182675bef8a8b49650c82a51
Uladzislau Khamkou <kham@tuta.io>
2022-03-19 22:46:25 +0300

    implement graph contraction

--- graphs.py
+++ graphs.py
@@ -6,5 +6,20 @@ def graph_inverse(graph):
     return G_inv
 
 
+def contract_graph(graph, groups):
+    v2g = {}
+    for i, group in enumerate(groups):
+        for u in group:
+            v2g[u] = i
+    for u in graph:
+        if u not in v2g:
+            v2g[u] = len(groups)
+            groups.append({u})
+    result = {u: set() for u in range(len(groups))}
+    for i, group in enumerate(groups):
+        result[i] = {v2g[v] for u in group for v in graph[u]} - {i}
+    return result
+
+
 if __name__ == "__main__":
     print("Non! Vous n'utilisez pas le module graphique correctement.")
